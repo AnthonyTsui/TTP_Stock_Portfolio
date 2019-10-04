e6aba201c38f8701f5e2d5cd14daa7a801592ca4
Anthony <atsui4688@gmail.com>
2019-10-03 20:14:05 -0400

    Changed port settings

--- bin/www.js
+++ bin/www.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const app = require('../app.js'); // The express app we just created
 
-const port = parseInt(process.env.PORT, 10) || 8000;
+const port = (process.env.PORT|| 8000);
 app.set('port', port);
 
 const server = http.createServer(app);
